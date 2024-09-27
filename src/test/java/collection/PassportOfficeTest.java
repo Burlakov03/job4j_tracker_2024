02bba40df52fbf0b90079811accf9caf2e13783b
Burlakov03 <bf.karatel@gmail.com>
2024-09-27 12:31:58 +0800

    1. Паспорт и жители [#212656 #364517]

---
+++ src/test/java/collection/PassportOfficeTest.java
@@ -0,0 +1,24 @@
+package collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenAddDublicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        Citizen citizen2 = new Citizen("2f44a", "VladimirBurlakov");
+        assertThat(office.add(citizen2)).isFalse();
+    }
+}
