0219cd5740daffb08ebb7d3a497ec3684a911365
Burlakov03 <bf.karatel@gmail.com>
2024-10-25 21:48:09 +0800

    7. Уникальность элементов и сортировка. [#110225 #371160]

--- src/main/java/stream/Profiles.java
+++ src/main/java/stream/Profiles.java
@@ -1,11 +1,20 @@
 package stream;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .toList();
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .distinct()
+                .sorted(Comparator.comparing(Address::getCity))
                 .toList();
     }
 }
