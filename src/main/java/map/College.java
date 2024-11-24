a1d6ebe90a6e2fcdc7a30612357714587e138365
Burlakov03 <bf.karatel@gmail.com>
2024-11-24 21:16:38 +0800

    3. Optional - empty, of [#214711 #371785]

--- src/main/java/map/College.java
+++ src/main/java/map/College.java
@@ -1,6 +1,7 @@
 package map;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class College {
@@ -11,38 +12,19 @@ public class College {
         this.students = students;
     }
 
-    public Student findByAccount(String account) {
+    public Optional<Student> findByAccount(String account) {
         return students.keySet()
                 .stream()
                 .filter(p -> p.account().equals(account))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
     }
 
-    public Subject findBySubjectName(String account, String name) {
-        Student student = findByAccount(account);
-        if (student == null) {
-            return null;
-        }
-        return students.get(student)
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Student> a = findByAccount(account);
+        return a.flatMap(student -> students.get(student)
                 .stream()
-                .filter(p -> p.name().equals(name))
-                .findFirst()
-                .orElse(null);
+                .filter(s -> s.name().equals(name))
+                .findFirst());
     }
 
-    public static void main(String[] args) {
-        Map<Student, Set<Subject>> students = Map.of(new Student("Student", "000001", "201-18-15"),
-                Set.of(
-                        new Subject("Math", 70),
-                        new Subject("English", 85)
-                )
-        );
-        College college = new College(students);
-        Student student = college.findByAccount("000001");
-        System.out.println("Найденный студент: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
-        System.out.println("Оценка по найденному предмету: " + english.score());
-
-    }
 }
